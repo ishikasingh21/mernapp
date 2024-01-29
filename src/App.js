0c67399825d000c961a2f1c3f78bebbebda950fb
Ishika Singh <144758046+ishikasingh21@users.noreply.github.com>
2024-01-29 21:51:24 +0530

    made new files

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import Home from './screens/Home';
 import{
   BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+  Routes,
+  Route
 } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <div> <Home></Home></div>
-    </>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home/>}> </Route>
+
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
