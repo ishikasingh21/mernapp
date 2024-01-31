7c863358575a447f67b5957923285af345edceb2
Ishika Singh <144758046+ishikasingh21@users.noreply.github.com>
2024-01-31 21:22:17 +0530

    card properties

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,21 +6,11 @@ export default function Home() {
     return (
         <div>
             <div> <Navbar /> </div>
-            <div><div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px"}}>
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-copy">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is some IMPORTANT text.</p>
-                    <div className='container w-100'></div>
-                    <select className='m-2 h-100 w-100bg-success'>
-                        {Array.from(Array(6), (i)=>{
-                            return(
-                                <option key={i+1}> value={i+1} </option>
-                            )
-                        })}
-                    </select>
-                </div>
-            </div></div>
+
+            <div>
+                <Card />
+            </div>
+
             <div><Footer /> </div>
         </div>
     )
