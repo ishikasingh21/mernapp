39e1cdea871116c7bc0f6396f3395f221a15b84f
Ishika Singh <144758046+ishikasingh21@users.noreply.github.com>
2024-02-02 19:49:18 +0530

    updated card, footer, home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,32 +6,9 @@ export default function Home() {
     return (
         <div>
             <div> <Navbar /> </div>
-
-            <div><div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-copy">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is some IMPORTANT text.</p>
-                    <div className='container w-100'>
-                        <select className='m-2 h-100 w-100bg-success rounded'>
-                            {Array.from(Array(6), (e, i) => {
-                                return (
-                                    <option key={i + 1}> value={i + 1} </option>
-                                )
-                            })}
-                        </select>
-
-                        <select className='m-2 h-100 w-100bg-success rounded'>
-                            <option value='half'>Half</option>
-                            <option value='full'>Full</option>
-                        </select>
-                        <div className='d-inline h-100 fs-5'>
-                            Total Price
-                        </div>
-                    </div>
-                </div>
-            </div></div>
-
+            <div>
+                <card />
+            </div>
             <div><Footer /> </div>
         </div >
     )
