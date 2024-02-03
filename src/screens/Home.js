805dc59aeb56b57ac4f29ec373f49f3058fe86a3
Ishika Singh <144758046+ishikasingh21@users.noreply.github.com>
2024-02-03 18:58:12 +0530

    update components

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Carousel from '../components/Carousel'
 
 export default function Home() {
     return (
         <div>
             <div> <Navbar /> </div>
+            <div><Carousel /></div>
             <div>
                 <card />
             </div>
